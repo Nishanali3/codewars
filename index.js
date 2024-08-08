6edfe8b5abbcb8344d679c316e83fc86c34e1fb0
Nishanali3 <nishanaliabdiashim757@gmail.com>
2024-08-08 22:41:23 +0500

    solve tasks

--- index.js
+++ index.js
@@ -100,3 +100,136 @@ var replaceDots = function(str) {
 // replaceDots("one.two.three")
 
 const grow = (x) => x.reduce((acc, cv)=> acc * cv)
+
+function findMultiples(int, limit) {
+
+  let arr = [];
+  
+    for (let i=int; i<=limit; i+=int){
+      arr.push(i)
+    }
+
+  return arr
+  
+}
+
+const quarterOf = (month) => {
+  return Math.ceil(month/3)
+ }
+
+ function typeOfSum(a, b) {
+  return (typeof (a+b))
+}
+
+function findDifference(a, b) {
+
+  let getA = a.reduce((a,b)=> a*b)
+  let getB = b.reduce((a,b)=> a*b)
+
+  return Math.abs(getA - getB)
+
+}
+
+function datingRange(age){
+  let over14 = `${Math.floor((age/2)+7) }-${Math.floor((age-7)*2) }`
+  let at14under = `${Math.floor(Math.abs( (age*.10)-age))}-${Math.floor((age*.10)+age)}`
+  
+  return(age<=14)? at14under : over14
+
+}
+
+function warnTheSheep(queue) {
+
+  let get = queue.indexOf("wolf") 
+  let l = queue.length
+  let ans = (l-get)-1
+  
+  
+ return (queue.pop() === "wolf") ?"Pls go away and stop eating my sheep" 
+  :`Oi! Sheep number ${ans}! You are about to be eaten by a wolf!`
+  
+}
+
+var summation = function (num) {
+  let arr = []
+
+    while(num >= 1){
+      arr.push(num)
+      num--
+    }
+
+  return arr.reduce((a,b)=> a+b)
+    
+}
+
+function whatday(num) { 
+  switch(num) {
+    case 1: return "Sunday";
+    case 2: return "Monday";
+    case 3: return "Tuesday";
+    case 4: return "Wednesday";
+    case 5: return "Thursday";
+    case 6: return "Friday";
+    case 7: return "Saturday";
+    default: return 'Wrong, please enter a number between 1 and 7';
+  }
+}
+
+function finalGrade (exam, proj) {
+
+  return (exam > 90 || proj > 10) ? 100 
+    : (exam > 75 && proj >= 5) ? 90 
+    : (exam > 50 && proj >= 2) ? 75
+    : 0
+
+}
+
+function weatherInfo (temp) {
+
+  var cel = (temp - 32) * (5/9)
+  
+  return (cel <= 0)? `${cel} is freezing temperature` : `${cel} is above freezing temperature`
+  
+  }
+
+  function testEven(n) {
+    return (n%2 == 0)? true : false
+ }
+//  testEven(1)
+
+function isOpposite(s1,s2){
+
+  if(!s1.length || s1.length !== s2.length){
+    return false
+  }else{
+    
+    let lo = s1.split('').map(a => a == a.toLowerCase())
+    let up = s2.split('').map(a => a == a.toUpperCase())
+  
+    return lo.every((a,i)=> a === up[i])
+  
+  
+  }
+}
+
+function solution(a, b){
+  return (b.length > a.length) ? `${a}${b}${a}` : `${b}${a}${b}`
+}
+
+function move (pos, roll) {
+  return pos + roll * 2 
+}
+
+function checkForFactor (base, factor) {
+  return (base % factor === 0) ? true : false
+}
+
+// checkForFactor(653,7)
+
+function powersOfTwo(n){
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
